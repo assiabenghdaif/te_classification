b350b533afd9ca8d0329f9daa64383ca79bda9d3
Assia Benghdaif <99361390+assiabenghdaif@users.noreply.github.com>
2024-09-10 11:26:23 +0100

    ok

--- backend/server.py
+++ backend/server.py
@@ -7,9 +7,7 @@ from tensorflow.keras.models import load_model
 
 
 app = Flask(__name__)
-# tfidf_vectorizer=load_model("models/tfidf_vectorizer.joblib")
-# SvM_model=load_model("models/SVM_classifier_poly_2.joblib")
-# modelCNN = load_modelCNN()
+
 label_encoder = load_PKL("models/label_encoder_for_dna_ML_CNN_GRU_onehot.pkl")
 cnn_gru_model=load_modelCNN_GRU("models/model_weights_dna_ML_CNN_GRU_onehot_v3.weights.h5")
 lstm_model = load_modelLSTM()
@@ -20,10 +18,7 @@ k=3
 def receive_data():
     try:
         
-        # rf_model_20.joblib
-        
-        # kmeans_model=joblib.load("models/kmeans_model.joblib")
-        # tfidf_vectorizer=joblib.load("models/tfidf_vectorizer.joblib")
+       
         # Assuming the data is sent as JSON
         received_data = request.get_json()
         sequences = received_data  # The received_data is now the list directly
@@ -31,7 +26,7 @@ def receive_data():
         sequences = [s.lower() for s in sequences]
         # Process the received data as needed
         # print("Received data:", sequences)
-        # if(preprocessing_LSTM_CNN_GRU(sequences[0])==True):
+       
             
 
             # for LSTM
@@ -49,21 +44,7 @@ def receive_data():
         sequences_classe=clusterDetected(sequences_label[0])
         print(sequences_pred_proba)
         print(sequences_classe)
-            # for SVM
-            # sequences_X=preprocessing(sequences)
-            # print(sequences)
-            # sequences_X_vecto=Vectorize(sequences_X,tfidf_vectorizer)
-            # print(sequences_X_vecto)
-            # print(sequences_X_vecto.shape)
-            # print(type(sequences_X_vecto))
-            # sequences_pred = predict(SvM_model,sequences_X_vecto)
-            # sequences_pred_proba = predict_proba(SvM_model,sequences_X_vecto)
-            # sequences_pred_proba=proba_SVM(sequences_pred_proba)
-            # # sequences_classe=[]
-            # # for seq in sequences_pred:
-            # sequences_classe=clusterDetected(sequences_pred)
-            # print(sequences_pred_proba)
-            # print(sequences_classe)
+            
                     
             
         return jsonify({'message': 'Data received successfully','sequences_classe': sequences_classe,'sequences_pred_proba':sequences_pred_proba}), 200
@@ -78,24 +59,17 @@ def receive_data():
 def SuperFamilies():
     try:
         
-        # rf_model_20.joblib
         
-        # kmeans_model=joblib.load("models/kmeans_model.joblib")
-        # tfidf_vectorizer=joblib.load("models/tfidf_vectorizer.joblib")
         # Assuming the data is sent as JSON
         received_data = request.get_json()
         sequences = received_data  # The received_data is now the list directly
-        # print(sequences)
+        
         sequences = received_data.get('sequences', [])
-        # similarity = received_data.get('similarity', 0)
-        # emailConnect = received_data.get('emailConnect','')
+        
         
         print(sequences)
         sequences = [s.replace('\n', '') for s in sequences]
-        # Process the received data as needed
-        # print("Received data:", sequences)
-        # if(preprocessing_LSTM_CNN_GRU(sequences[0])==True):
-        # print(sequences)
+        
 
         cleaned_sequences = [clean_sequence(seq) for seq in sequences]
         # print(cleaned_sequences)
@@ -111,24 +85,10 @@ def SuperFamilies():
         classesDetected_name=classesDetected(predicted_label)
         # classesDetected_name="PiggyBac"
 
-            # d=get_map(sequences[0])
-            # get_tirs(d,sequences,similarity)
-            # sender_email = 'benghdaif.assia2001@gmail.com'
-            # smtp_user = 'assiabenghdaif@gmail.com'
-            # smtp_password = 'nmae anyx krbf tjhh'  # App-specific password if 2FA is enabled
-
-            # email_sender = EmailSender(sender_email, smtp_user, smtp_password)
-
-            # # receiver_email = 'assiabenghdaif@gmail.com'
-            # subject = 'TIRs Localisation'
-            # body = 'This files contains the results of TIRs localisation of your sequence.'
-            # filenames = ['ETs_TSD.txt', 'ETs_withoutTSD.txt']  # List of files to attach
-            # email_sender.send_email(emailConnect, subject, body, filenames)
+            
             
         return jsonify({'message': 'Data received successfully','sequences_family': classesDetected_name,'sequences_pred_proba_family':res_percentage_cnn_gru}), 200
-        # else :
-        #     return jsonify({'message': 'Data received successfully','sequences_family': "min 1300",'sequences_pred_proba_family':"min 1300"}), 200
-
+        
         
     except Exception as e:
         print("Error processing data:", str(e))
@@ -152,7 +112,6 @@ def TIRLocalisation():
         sequences = [s.replace('\n', '') for s in sequences]
         # Process the received data as needed
         # print("Received data:", sequences)
-        # if(preprocessing_LSTM_CNN_GRU(sequences[0])==True):
             
         d=get_map(sequences[0])
         get_tirs(d,sequences[0],similarity)
@@ -177,36 +136,4 @@ def TIRLocalisation():
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
-
-
-
-
-
-
-# CNN
-
-
-
-
-
-            # sequences_X=preprocessing(sequences)
-            # print(sequences)
-            # sequences_X_vecto=Vectorize(sequences_X,tfidf_vectorizer)
-            # print(sequences_X_vecto)
-            # print(sequences_X_vecto.shape)
-            # print(type(sequences_X_vecto))
-            
-            # sequences_pred_proba_family = predict_cnn_proba(modelCNN,sequences_X_vecto)
-            # res_percentage = [[f"{i * 100:.2f}%" for i in inner] for inner in sequences_pred_proba_family]
-
-            # print(res_percentage)
-            # sequences_pred = predict_cnn_class(sequences_pred_proba_family)
-            # ii=labelsFamilles(sequences_pred)
-            # sequences_family=classesDetected(ii)
-            # print(sequences_pred_proba_family)
-            # print(type(sequences_pred_proba_family))
-            # print(sequences_pred)
-            # print(sequences_family)
-            # sequences_pred_proba_family=proba_CNN(sequences_pred_proba_family)
             # print(sequences_pred_proba_family)
